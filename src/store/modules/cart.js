7b6ee623eb899455f933fcd4827efddc2040662a
Yin Kewei <yinkewei@YindeMacBook-Air.local>
2021-12-04 22:46:43 +0800

    init

---
+++ src/store/modules/cart.js
@@ -0,0 +1,10 @@
+/* 购物车模块 */
+export default {
+  namespaced: true,
+
+  state: () => {
+    return {
+      list: [],
+    };
+  },
+};
---
+++ src/store/modules/user.js
@@ -0,0 +1,18 @@
+/* 用户模块 */
+export default {
+  namespaced: true,
+
+  state() {
+    return {
+      // 用户信息
+      profile: {
+        id: "",
+        avatar: "",
+        nickname: "",
+        account: "",
+        mobile: "",
+        token: "",
+      },
+    };
+  },
+};
